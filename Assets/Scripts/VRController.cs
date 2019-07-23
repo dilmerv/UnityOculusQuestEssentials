f079a13e8c429f81ee0e54dcf83adb90ae1d1760
Dilmer Valecillos <dilmervst@gmail.com>
2019-07-22 21:57:59 -0600

    Added VR Player controller with run and jump mechanic

---
+++ Assets/Scripts/VRController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(OVRPlayerController))]
+public class VRController : MonoBehaviour
+{
+    private OVRPlayerController controller;
+
+    [SerializeField]
+    private float moveSpeedMultiplier = 3.0f;
+
+    [SerializeField]
+    private bool allowDoubleXSpeed = false;
+
+    void Start()
+    {
+        controller = GetComponent<OVRPlayerController>();
+        controller.SetMoveScaleMultiplier(moveSpeedMultiplier);
+    }
+
+    void Update()
+    {
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            controller.Jump();
+        }
+
+        if(OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) && allowDoubleXSpeed)
+        {
+            controller.SetMoveScaleMultiplier(moveSpeedMultiplier * 2.0f);
+        }
+        else 
+        {
+            controller.SetMoveScaleMultiplier(moveSpeedMultiplier);
+        }
+    }
+}
